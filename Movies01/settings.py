4e534a7efbca93f427040455d2b997d6339a74b9
Tiago de França Queiroz <contato@tiago.eti.br>
2014-05-12 21:47:26 -0300

    fixing settings

--- Movies01/settings.py
+++ Movies01/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 }
 
 #Heroku
-if os.environ['USER'] != 'belimawr':
+if os.environ.get('USER') is None:
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
